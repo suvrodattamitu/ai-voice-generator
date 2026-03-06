37fc288192a4a45c9bd6932cc5f95fe4975f9070
Suvro Datta <suvrodatta95@gmail.com>
2026-03-06 09:43:41 +0600

    added dashboard view

---
+++ src/features/dashboard/views/dashboard-views.tsx
@@ -0,0 +1,20 @@
+import { PageHeader } from "@/components/page-header";
+import { HeroPattern } from "@/features/dashboard/components/hero-pattern";
+import {DashboardHeader} from "@/features/dashboard/components/dashboard-header";
+import { TextInputPanel } from "../components/text-input-panel";
+import { QuickActionsPanel } from "@/features/dashboard/components/quick-actions-panel";
+
+export function DashboardView() {
+    return (
+        <div className="relative">
+            <PageHeader title="Home" className="lg:hidden"/>
+            <HeroPattern/>
+
+            <div className="relative space-y-8 p-4 lg:p-16"> 
+                <DashboardHeader />
+                <TextInputPanel />
+                <QuickActionsPanel />
+            </div>
+        </div>
+    )
+}
