907357fb94ca62e82e33c146182b9f48d7ee98d2
Suvro Datta <suvrodatta95@gmail.com>
2026-03-07 22:04:51 +0600

    added design of settings panel

---
+++ src/features/text-to-speech/components/text-to-speech-form.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {z} from "zod";
+import { formOptions, FormOptions } from "@tanstack/react-form";
+import { useAppForm } from "@/hooks/use-app-form";
+
+const ttsFormSchema = z.object({
+    text: z.string().min(1, "Please enter some text"),
+    voiceId: z.string().min(1, "Please select a voice"),
+    temparature: z.number(),
+    topP: z.number(),
+    topK: z.number(),
+    repetitionPenalty: z.number()
+});
+
+export type TTSFormValues = z.infer<typeof ttsFormSchema>;
+
+export const defaultTTSValues: TTSFormValues = {
+    text: "",
+    voiceId: "",
+    temparature: 0.8,
+    topP: 0.95,
+    topK: 1000,
+    repetitionPenalty: 1.2 
+}
+
+export const ttsFormOptions = formOptions({
+    defaultValues: defaultTTSValues
+});
+
+export function TextToSpeechForm({
+    children, defaultValues
+}: {
+    children: React.ReactNode,
+    defaultValues?: TTSFormValues
+}) {
+    const form = useAppForm({
+        ...ttsFormOptions,
+        defaultValues: defaultValues ?? defaultTTSValues,
+        validators: {
+            onSubmit: ttsFormSchema
+        },
+        onSubmit: async () => {
+
+        }
+    })
+
+    return <form.AppForm>{children}</form.AppForm>
+};
